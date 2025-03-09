338c6ee24b6871821d314b1ed0e7fd1c5a618955
farinasm <62609159+farinasm@users.noreply.github.com>
2025-03-09 20:23:20 +0100

    docs

--- boolevard/io.py
+++ boolevard/io.py
@@ -86,28 +86,35 @@ class BooLEV:
         """
         Extracts drivers from (N)DNFs based on the local state of the node within a given stable state.
         
-        Args:
+        Parameters:
+        ********
+        ss: int 
+            Stable State to evaluate.
 
-        - model_info (pd.DataFrame): Info attribute of the BooLEV object.
-        - ss (int): Stable State to evaluate.
+        Returns:
+        ********
+        Dict
+            Dictionary containing de drivers of each node within the stable state.
 
-        Returns
-            - Dict: Dictionary containing de drivers of each node within the stable state.
+        Example:
+        ********
+        >>> model = blv.Load("model.bnet")
+        >>> model.Drivers(1) # Compute the drivers for each node within the stable state number 1
         """
         return Drivers(self.Info, ss)
 
     def CountPaths(self, tNodes:list, ss_wise = False):
-        '''
+        """
         Calculates the signed path count leading to the local state of a node contained in a list. Positive if the local state is 1 and negative if 0.
         
         Parameters:
         **********
         tNodes: list
-            list of target nodes to evaluate.
+            List of target nodes to evaluate.
         ss_wise: bool
-            if True, returns a list with the corresponding path counts leading to a target's local state for each stable state. Otherwise, it computes the average path count across all stable states contained in the Info attribute of the BooLEV object. By default: False.
+            If True, returns a list with the corresponding path counts leading to a target's local state for each stable state. Otherwise, it computes the average path count across all stable states contained in the Info attribute of the BooLEV object. By default: False.
 
-        Returns
+        Returns:
         **********
         list
             Signed number of paths leading to the local states of the target nodes. Negative if the local state is 0, positive if 1.
@@ -116,26 +123,35 @@ class BooLEV:
         **********
         >>> model = blv.Load("model.bnet")
         >>> model.CountPaths(["Node1, "Node2"], ss_wise = False)
-        '''
+        """
         return CountPaths(self.Info, tNodes, ss_wise)
     
     def Pert(self, perturbation:str, additive = True):
-        '''
-        Perturb the model by creating a perturbation node that targets a specific node in the model, simulating a positive (ACT) or negative (INH) effect in the target.
+        """
+        Perturbs the model by creating a perturbation node that targets a specific node in the model, simulating a positive (ACT) or negative (INH) effect in the target.
         
-        Arguments
-        ---------
-        model (obj): BooLEV object.
-        tNodes (list): list of target nodes to evaluate.
-        perturbation (str): string containing the target node and the perturbation type separated by a percentage symbol. E.g. "Node%ACT", "Node%INH".
-        additive (bool): if True, the perturbation is additive (i.e. the regulation is incorporated to the target node's rule). Otherwise, the perturbation is substitutive (i.e. the regulation replaces the target node's rule). By default: True    ---------
+        Parameters:
+        ********
+        model: obj
+            BooLEV object.
+        tNodes: list
+            List of target nodes to evaluate.
+        perturbation: str
+            String containing the target node and the perturbation type separated by a percentage symbol. E.g. "Node%ACT", "Node%INH".
+        additive: bool
+            If True, the perturbation is additive (i.e. the regulation is incorporated to the target node's rule). Otherwise, the perturbation is substitutive (i.e. the regulation replaces the target node's rule). By default: True    ---------
 
-        Returns
-        ---------
+        Returns:
+        ********
         BooLEV object
             Model (BooLEV object) updated with the perturbation.
-        ---------
-            '''
+        
+        Example:
+        ********
+        >>> model = blv.Load("model.bnet")
+        >>> model.Pert("Node%ACT", additive = True)
+        >>> model.Pert("Node%INH", additive = False)
+        """
         return Pert(self, perturbation, additive)
     
 
